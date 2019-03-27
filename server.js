39c9517ac9117fb4c69e5d35e75c1e196521dbd5
Ronnie Geraghty <ronniegerag@gmail.com>
2019-03-27 13:14:56 -0400

    preparing project for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -12,15 +13,24 @@ app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to Mongo 
+// Connect to Mongo
 mongoose
-    .connect(db, {dbName: 'mern_shopping', useNewUrlParser: true})
-    .then(() => console.log('MongoDB Connected...') )
-    .catch(err => console.log('ERROR',err));
+	.connect(db, { dbName: 'mern_shopping', useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected...'))
+	.catch((err) => console.log('ERROR', err));
 
-
-//Use routes 
+//Use routes
 app.use('/api/items', items);
+
+//Serve Static assets if were in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
